e57aa6c032bb6011ff4cc4e62759ae90584b6179
salamidrus <idrussalam95gmail.com>
2020-11-22 09:19:06 +0700

    [Idrus] - Selamat

--- tes.js
+++ tes.js
@@ -1,3 +1,4 @@
-console.log("test")
-console.log ("seli, putri, amanda")
-console.log ("bisa yu bisa")
+console.log("test");
+console.log("seli, putri, amanda");
+console.log("bisa yu bisa");
+console.log("Selamat siang");
